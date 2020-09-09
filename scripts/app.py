016954417199d001ae0b741bd770ae2f58275cec
Logan Bonsignore <Loganbon14@gmail.com>
2020-09-09 17:42:52 -0600

    add name&zip search

--- scripts/app.py
+++ scripts/app.py
@@ -16,11 +16,12 @@ def index():
     return (
         "<h1 style='text-align: center;'>Foodies DB</h1><br/>"
         "<div style='text-align: center;'>"
-        "<strong>Search by Name:</strong> /api/v1.0/search/name/{restaurant_name}<br/><br/>"
-        "<strong>Search by City:</strong> /api/v1.0/search/city/{city_name}<br/><br/>"
-        "<strong>Search by Name and City:</strong> /api/v1.0/search/name_city/{restaurant_name}/{city_name}<br/><br/>"
-        "<strong>Search by Zip Code:</strong> /api/v1.0/search/zipcode/{zip_code}<br/><br/>"
-        "<strong>Search by Phone Number:</strong> /api/v1.0/search/phone/{phone_number}<br/><br/>"
+        "<strong>Search by Name:</strong> /api/v1.0/search/name/{restaurant name}<br/><br/>"
+        "<strong>Search by City:</strong> /api/v1.0/search/city/{city name}<br/><br/>"
+        "<strong>Search by Name and City:</strong> /api/v1.0/search/name_city/{restaurant name}/{city name}<br/><br/>"
+        "<strong>Search by Zip Code:</strong> /api/v1.0/search/zipcode/{zip code}<br/><br/>"
+        "<strong>Search by Name and Zip Code:</strong> /api/v1.0/search/name_zip/{restaurant name}/{zip code}<br/><br/>"
+        "<strong>Search by Phone Number:</strong> /api/v1.0/search/phone/{phone number}<br/><br/>"
         "</div>"
     )
 
@@ -30,10 +31,10 @@ def search_by_name(name):
     name = name.lower()
     name = re.sub("\'(.*)", "", name)
     name = name.replace(" ","-")
-    # search all alias names for match
     return_list = []
-    # retrieve all records in db
+    # retrieve all restaurants in db
     for restaurant in db.yelp.find():
+        # search all alias names for match
         if name in restaurant["alias"]:
             # remove mongodb's "_id" field (incompatable with jsonify)
             restaurant.pop("_id")
@@ -61,10 +62,10 @@ def search_by_name_city(name, city):
     name = re.sub("\'(.*)", "", name)
     name = name.replace(" ","-")
     city = city.title()
-    # search all alias names for match
     return_list = []
-    # retrieve all records in db
+    # retrieve all restaurant in city
     for restaurant in db.yelp.find({"location.city":city}):
+        # search all alias names for match
         if name in restaurant["alias"]:
             # remove mongodb's "_id" field (incompatable with jsonify)
             restaurant.pop("_id")
@@ -83,12 +84,29 @@ def search_by_zipcode(zipcode):
     
     return jsonify(return_list)
 
+@app.route("/api/v1.0/search/name_zip/<name>/<zipcode>")    
+def search_by_name_zip(name, zipcode):
+    # normalize variables for optimal search results
+    name = name.lower()
+    name = re.sub("\'(.*)", "", name)
+    name = name.replace(" ","-")
+    return_list = []
+    # retrieve all restaurant in city
+    for restaurant in db.yelp.find({"location.zip_code":zipcode}):
+        # search all alias names for match
+        if name in restaurant["alias"]:
+            # remove mongodb's "_id" field (incompatable with jsonify)
+            restaurant.pop("_id")
+            return_list.append(restaurant)
+
+    return jsonify(return_list)
+
 @app.route("/api/v1.0/search/phone/<phone>")
 def search_by_phone(phone):
     # add country code to phone number (US phone numbers only)
     return_list = []
     phone = f"+1{phone}"
-    # retrieve all restaurants with the same phone number
+    # retrieve all restaurants with matching phone number
     for restaurant in db.yelp.find({"restaurant_phone":phone}):
         # remove mongodb's "_id" field (incompatable with jsonify)
         restaurant.pop("_id")
