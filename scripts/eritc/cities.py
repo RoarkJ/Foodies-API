ef38bf819e969849ffe59d71f2d6bd6380eeb90b
Bryan <30875957+RoarkJ@users.noreply.github.com>
2020-09-10 17:13:33 -0600

    readmedocumentation and script cleanup

--- scripts/eritc/cities.py
+++ scripts/eritc/cities.py
@@ -1,14 +1,9 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as bs
-import re
 import time
 import pymongo
 
 
-
-#	html=urlopen('https://everyrestaurantinthecity.com/colorado-restaurants/count')
-#	soup=bs(html, 'html.parser')
-#	for rows in soup.find_all('td', width='33%'):
 links={}
 unique=True
 count=-20
@@ -30,10 +25,7 @@ while unique == True:
 		except:
 			unique = False
 			print('Last Page Reached')
-			
 
-#for key, value  in links.items():
-#	print(f'{key}: {value}')
 
 # Initialize PyMongo to work with MongoDBs
 conn = 'mongodb://127.0.0.1:27017'
@@ -51,28 +43,5 @@ for key, value in links.items():
 
 
 
-
-'''
-while unique == True:
-	count += 20
-	url=f'https://everyrestaurantinthecity.com/colorado-restaurants/{count}'
-	print(f'This is the url: {url}')
-	time.sleep(10)
-	try:
-		html=urlopen(url)
-		time.sleep(3)
-		soup=bs(html, 'html.parser')
-		for rows in soup.find_all('td', width='33%'):
-			for row in rows.find_all('a'):
-					links.append(row.text)
-	except:
-		unique = False
-		print('Last Page Reached')
-
-for link in links:
-	print(link)
-	print('\n\n')
-
-'''
 	
 
